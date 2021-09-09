976c22c174b0c5e701be58bfd6e24d484729056c
Paul Kolesa <12650383+m4xx3d0ut@users.noreply.github.com>
2021-09-09 16:17:00 -0700

    Created Server/nginxMainlineLatest.py module for dowloading latest copy of Nginx from nginx.org.

---
+++ Server/nginxMainlineLatest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Downloads the most recent mainline version of Nginx from nginx.org and
+saves it locally.
+"""
+
+import requests 
+
+nginxDowloadUrl = 'http://nginx.org/en/download.html'
+
+def nginxMainlineLatest():
+
+    print('[*] Dowloading latest Nginx mainline from %s' % (nginxDowloadUrl))
+    r = requests.get(nginxDowloadUrl) 
+    status_code = str(r.status_code)
+    if str(r.status_code) == '200':
+        print('[*] Pass. Server status code: %s' % (status_code))
+    else:
+        print('[!] Error. Server status code: %s' % (status_code))
+        print('[!] Unable to download. Exiting...')
+        exit(0)
+    txt_split = r.text.split('"/download/nginx-')[1] 
+    ver = txt_split.split('.tar.gz')[0] 
+    local_file = 'nginx-%s.tar.gz' % (ver) 
+    remote_url = 'http://nginx.org/download/%s' % (local_file) 
+    print('[*] Downloading %s from %s' % (local_file, remote_url))
+    data = requests.get(remote_url) 
+    print('[+] Writing to local file: %s' % (local_file))
+    with open(local_file, 'wb') as file: 
+        file.write(data.content) 
+    print('[!] Dowload complete.')
+
+if __name__ == '__main__':
+
+    nginxMainlineLatest()
