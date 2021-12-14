458b05fa4f3f58387867a64bcbf8b4a252f79a49
Paul Kolesa <12650383+m4xx3d0ut@users.noreply.github.com>
2021-12-14 12:50:48 -0800

    Added WSGI entry point, test_uwsgi.sh script, and updated requirements.txt.

--- app/__init__.py
+++ app/__init__.py
@@ -4,50 +4,39 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session.__init__ import Session
-# from werkzeug.utils import secure_filename
-
-
-"""
-So, there is a lot of stuff commented out here.  That's because I took this from
-one of my other projects and won't be using all of this at first, but I will
-need it sooner or later.
-"""
 
 
 db = SQLAlchemy()
 sess = Session()
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Flask session
-    app.config['SESSION_COOKIE_NAME'] = 'session_cookie'
-    app.config['SESSION_TYPE'] = os.environ.get('SESSION_TYPE')
-    app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('SESSION_REDIS'))
+# Flask session
+app.config['SESSION_COOKIE_NAME'] = 'session_cookie'
+app.config['SESSION_TYPE'] = os.environ.get('SESSION_TYPE')
+app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('SESSION_REDIS'))
 
-    db.init_app(app)
-    sess.init_app(app)
+db.init_app(app)
+sess.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    from .models import User
+from .models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    # # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+# # blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
